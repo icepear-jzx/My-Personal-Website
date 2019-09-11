40a38e355b6a3e223f157dbba3d93d9d16d16d55
IcePear-Jzx <icepear.jzx@gmail.com>
2019-09-11 21:31:56 +0800

    add a demo

--- flask-tutorial/flaskr/__init__.py
+++ flask-tutorial/flaskr/__init__.py
@@ -32,4 +32,11 @@ def create_app(test_config=None):
     from . import db
     db.init_app(app)
 
+    from . import auth
+    app.register_blueprint(auth.bp)
+
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
+
     return app
