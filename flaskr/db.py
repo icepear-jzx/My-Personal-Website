f5cdb13c07850099710bc51a2d12db30e8d9784b
IcePear-Jzx <icepear.jzx@gmail.com>
2019-09-25 11:21:15 +0800

    update

--- flaskr/db.py
+++ flaskr/db.py
@@ -1,5 +1,3 @@
-import sqlite3
-
 import click
 from flask import current_app, g
 from flask.cli import with_appcontext
--- flaskr/plans.py
+++ flaskr/plans.py
@@ -1,9 +1,9 @@
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
-from werkzeug.security import check_password_hash, generate_password_hash
 
 from flaskr.db import get_db
+
 
 bp = Blueprint('plans', __name__, url_prefix='/plans')
 
